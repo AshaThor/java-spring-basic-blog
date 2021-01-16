7e33bf5f8dabc1fd88b66256d3a69be9f857b771
AshaThor <smartyparty101@hotmail.com>
2021-01-16 23:23:02 +0000

    Updates to show posts

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,16 +1,26 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
 
+    private PostRepository postRepository;
+
     @RequestMapping("/")
     public String listPosts(ModelMap mMap){
-        mMap.put("title","Blog Post 1");
+        List<Post> allPosts = postRepository.getAllPosts();
+        mMap.put("posts", allPosts);
         return "home";
     }
 
+    public void BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 }
