70df9cea3ddf1cee71c4fb3c36e60a585e983398
AshaThor <smartyparty101@hotmail.com>
2021-01-17 01:02:44 +0000

    Updates to show posts

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -13,14 +13,16 @@ public class BlogController {
 
     private PostRepository postRepository;
 
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
     @RequestMapping("/")
-    public String listPosts(ModelMap mMap){
-        List<Post> allPosts = postRepository.getAllPosts();
-        mMap.put("posts", allPosts);
+    public String listPosts(ModelMap modelMap){
+        List<Post> posts = postRepository.getAllPosts();
+        modelMap.put("posts", posts);
         return "home";
     }
 
-    public void BlogController(PostRepository postRepository) {
-        this.postRepository = postRepository;
-    }
+
 }
